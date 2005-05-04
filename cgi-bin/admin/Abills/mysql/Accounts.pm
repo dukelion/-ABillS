35e3f53b157a8b1c47b8cb4e39e0dd001b5f8c0e
abills <abills>
2005-05-04 09:12:03 +0000

    *** empty log message ***

--- cgi-bin/admin/Abills/mysql/Accounts.pm
+++ cgi-bin/admin/Abills/mysql/Accounts.pm
@@ -16,6 +16,8 @@ $VERSION = 2.00;
 @EXPORT_OK = ();
 %EXPORT_TAGS = ();
 
+use main;
+@ISA  = ("main");
 
 my $db;
 # Customer id
@@ -30,6 +32,7 @@ sub new {
   $db = shift;
   my $self = { };
   bless($self, $class);
+  $self->{debug}=1;
   return $self;
 }
 
@@ -90,33 +93,44 @@ sub change {
     return $self;
    }
 
-  my $tax_number  = (defined($attr->{TAX_NUMBER})) ? $attr->{TAX_NUMBER} : '';
-  my $bank_account = (defined($attr->{BANK_ACCOUNT})) ? $attr->{BANK_ACCOUNT} : '';
-  my $bank_name = (defined($attr->{BANK_NAME})) ? $attr->{BANK_NAME} : '';
-  my $cor_bank_account = (defined($attr->{COR_BANK_ACCOUNT})) ? $attr->{COR_BANK_ACCOUNT} : '';
-  my $bank_bic = (defined($attr->{BANK_BIC})) ? $attr->{BANK_BIC} : '';
+ my %DATA = $self->get_data($attr); 
+ my $CHANGES_QUERY = "";
+ my $CHANGES_LOG = "Account:";
 
-  my $sql = "UPDATE
-     accounts SET 
-     name='$name', 
-     tax_number='$tax_number', 
-     bank_account='$bank_account', 
-     bank_name='$bank_name', 
-     cor_bank_account='$cor_bank_account', 
-     bank_bic='$bank_bic'
-     WHERE id='$account_id';";
-  my $q = $db->do($sql); 
+#   DEPOST => 'deposit', 
+#   ACCOUNT_ID => 'id', 
+ my %FIELDS = (
+   ACCOUNT_NAME => 'name', 
+   TAX_NUMBER => 'tax_number', 
+   BANK_ACCOUNT => 'bank_account', 
+   BANK_NAME => 'bank_name', 
+   COR_BANK_ACCOUNT => 'cor_bank_account', 
+   BANK_BIC => 'bank_bic'
+   );
 
-  if ($db->err == 1062) {
-     $self->{errno} = 7;
-     $self->{errstr} = 'ERROR_DUBLICATE';
-     return $self;
-   }
-  elsif($db->err > 0) {
-     $self->{errno} = 3;
-     $self->{errstr} = 'SQL_ERROR';
-     return $self;
-   }
+ my $OLD = $self->info($account_id);
+
+ while(my($k, $v)=each(%DATA)) {
+    if ($OLD->{$k} ne $DATA{$k}){
+      if ($FIELDS{$k}) {
+         $CHANGES_LOG .= "$k $OLD->{$k}->$DATA{$k};";
+         $CHANGES_QUERY .= "$FIELDS{$k}='$DATA{$k}',";
+       }
+     }
+  }
+
+if ($CHANGES_QUERY eq '') {
+  return $self->{result};	
+}
+
+# print $CHANGES_LOG;
+  chop($CHANGES_QUERY);
+  $self->query($db, "UPDATE accounts SET $CHANGES_QUERY
+    WHERE id='$account_id';", 'do');
+
+#  $admin->action_add(0, "$CHANGES_LOG");
+
+  $self->info($account_id);
 
   return $self;
 }
@@ -128,10 +142,7 @@ sub change {
 sub del {
   my $self = shift;
   my ($account_id) = @_;
-
-  my $sql = "DELETE FROM accounts WHERE id='$account_id';";
-  my $q = $db->do($sql); 
-
+  $self->query($db, "DELETE FROM accounts WHERE id='$account_id';", 'do');
   return $self;
 }
 
@@ -143,25 +154,25 @@ sub info {
   my $self = shift;
   my ($account_id) = @_;
 
-  my $sql = "SELECT name, deposit, tax_number, bank_account, bank_name, cor_bank_account, bank_bic
-    FROM accounts WHERE id='$account_id';";
+  $self->query($db, "SELECT id, name, deposit, tax_number, bank_account, bank_name, cor_bank_account, bank_bic
+    FROM accounts WHERE id='$account_id';");
 
-  my $q = $db->prepare($sql) || die $db->errstr;
-  $q ->execute(); 
-
-  if ($q->rows < 1) {
+  if ($self->{TOTAL} < 1) {
      $self->{errno} = 2;
      $self->{errstr} = 'ERROR_NOT_EXIST';
      return $self;
    }
 
-  ($self->{ACCOUNT_NAME}, 
+  my $a_ref = $self->{list}->[0];
+
+  ($self->{ACCOUNT_ID}, 
+   $self->{ACCOUNT_NAME}, 
    $self->{DEPOST}, 
    $self->{TAX_NUMBER}, 
    $self->{BANK_ACCOUNT}, 
    $self->{BANK_NAME}, 
    $self->{COR_BANK_ACCOUNT}, 
-   $self->{BANK_BIC})= $q->fetchrow();
+   $self->{BANK_BIC}) = @$a_ref;
     
   return $self;
 }
@@ -180,32 +191,22 @@ sub list {
  my $PG = ($attr->{PG}) ? $attr->{PG} : 0;
  my $PAGE_ROWS = ($attr->{PAGE_ROWS}) ? $attr->{PAGE_ROWS} : 25;
 
- 
- my $q = $db->prepare("SELECT count(a.id) FROM accounts a;");
-
- $q ->execute(); 
- my ($total) = $q->fetchrow();
-
-# print "SELECT u.id, u.fio, u.deposit, u.credit, v.name, u.uid 
-#     FROM users u
-#     LEFT JOIN  variant v ON  (v.vrnt=u.variant) 
-#     $WHERE ORDER BY $SORT $DESC LIMIT $PG, $PAGE_ROWS;";
- 
-   $q = $db->prepare("SELECT a.name, a.deposit, count(u.uid), a.id
+ $self->query($db, "SELECT a.name, a.deposit, count(u.uid), a.id
     FROM accounts a
     LEFT JOIN users u ON (u.account_id=a.id)
     GROUP BY a.id
     ORDER BY $SORT $DESC LIMIT $PG, $PAGE_ROWS;");
+ my $list = $self->{list};
 
- $q ->execute(); 
- my @accounts = ();
- 
- while(my @account = $q->fetchrow()) {
-   push @accounts, \@account;
-  }
+ if ($self->{TOTAL} > 0) {
+    $self->query($db, "SELECT count(a.id) FROM accounts a;");
+    my $a_ref = $self->{list}->[0];
+    ($self->{TOTAL}) = @$a_ref;
+   }
 
-$self->{list} = \@accounts;
-return $self->{list}, $total;
+#  $self->{list}=$list;
+
+return $list;
 }
 
 
