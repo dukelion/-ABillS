a0d126b79885591f94cc4810e9e87f348e075b64
abills <abills>
2008-01-19 12:44:55 +0000

    *** empty log message ***

--- db/Extfin.sql
+++ db/Extfin.sql
@@ -7,11 +7,25 @@ CREATE TABLE `extfin_paids` (
   `aid` smallint(6) unsigned NOT NULL default '0',
   `status` tinyint(2) unsigned NOT NULL default '0',
   `type_id` smallint(6) unsigned NOT NULL default '0',
+  `maccount_id` tinyint(4) unsigned NOT NULL default '0', 
   `status_date` date NOT NULL default '0000-00-00',
   `ext_id` varchar(24) NOT NULL default '',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `id` (`id`)
 )  COMMENT='Extfin paids list';
+
+
+
+CREATE TABLE `extfin_money_accounts` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `date` date NOT NULL default '0000-00-00',
+  `name` varchar(60)  NOT NULL default '',
+  `comments` varchar(200) NOT NULL default '',
+  `expire` date NOT NULL default '0000-00-00',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `name` (`name`)
+) COMMENT='Extfin Money accounts list';
 
 
 CREATE TABLE `extfin_paids_periodic` (
@@ -22,6 +36,7 @@ CREATE TABLE `extfin_paids_periodic` (
   `date` date NOT NULL default '0000-00-00',
   `aid` smallint(6) unsigned NOT NULL default '0',
   `comments` varchar(100) NOT NULL default '',
+  `maccount_id` tinyint(4) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`)
 )  COMMENT='Extfin periodic paids';
 
