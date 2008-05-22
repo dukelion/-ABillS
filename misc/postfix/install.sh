9d383a7e5e80e2d2f1e8db7ed4038179f674a224
abills <abills>
2008-05-22 10:22:28 +0000

    *** empty log message ***

--- misc/postfix/install.sh
+++ misc/postfix/install.sh
@@ -1,19 +1,31 @@
 #!/bin/sh
 # make deamon accounts
-#
 # Make vmail user account
+#Make clamav user account
 
+if [ w$1 = w ]; then 
+  echo "Make accounts";
+  echo ""
+  echo "# mk_account.sh TYPE OS"
+  echo "TYPE - Account type"
+  echo "  Clamav"
+  echo "  ADD_VMAIL_USER"
+  echo "OS   - FREEBSD (default), LINUX"
+  echo "DEBUG"
+  echo ""
 
-if [ wLinux = w$1 ]; then
-  if [ wClamav = w$2 ]; then
+fi;
+
+if [ wLinux = w$2 ]; then
+  if [ wClamav = w$1 ]; then
     GROUP=clamav;
     USER=clamav;
     if /bin/id "${GROUP}" 2>/dev/null; then
       echo "Group Exist";
     else
-      /usr/sbin/groupadd -g 106 clamav
+      /usr/sbin/groupadd -g ${GID} clamav
     fi
-    /usr/sbin/useradd -c "Clam Antivirus" -d /nonexistent -s /sbin/nologin -u 106 -g 106 clamav 
+    /usr/sbin/useradd -c "Clam Antivirus" -d /nonexistent -s /sbin/nologin -u ${USER} -g ${GID} ${USER}
   fi
   exit;
 fi
@@ -25,11 +37,24 @@ else
 fi
 
 if [ x"$1" = xADD_VMAIL_USER ]; then
-        USER=vmail
-        UID=1005
-        GROUP=vmail
-        GID=1005
+   USER=vmail
+   UID=1005
+   GROUP=vmail
+   GID=1005
+#Linux 
 
+  if [ x"$1" = xLINUX ]; then
+    if /bin/id "${GROUP}" 2>/dev/null; then
+       echo "Group Exist";
+    else
+      /usr/sbin/groupadd -g ${GID} ${GROUP}
+    fi;
+
+    /usr/sbin/useradd -c "VMAIL" -d /nonexistent -s /sbin/nologin -u ${UID} -g ${GID} ${USER}
+
+  else  
+
+#FreeBSD Section
         if /usr/sbin/pw groupshow "${GROUP}" 2>/dev/null; then
                 echo "You already have a group \"${GROUP}\", so I will use it."
         else
@@ -54,6 +79,7 @@ if [ x"$1" = xADD_VMAIL_USER ]; then
                         exit 1
                 fi
         fi
+   fi;
 
 fi;
 
