80b38da4ea8d220fcdb298f93118957e801c9ec6
abills <abills>
2009-03-31 13:07:49 +0000

    *** empty log message ***

--- Abills/mysql/Acct.pm
+++ Abills/mysql/Acct.pm
@@ -306,19 +306,21 @@ elsif($acct_status_type eq 3) {
       status='$acct_status_type',
       nas_port_id='$RAD->{NAS_PORT}',
       acct_session_time=UNIX_TIMESTAMP()-UNIX_TIMESTAMP(started),
-      acct_input_octets='$RAD->{INBYTE}',
-      acct_output_octets='$RAD->{OUTBYTE}',
-      ex_input_octets='$RAD->{INBYTE2}',
-      ex_output_octets='$RAD->{OUTBYTE2}',
       framed_ip_address=INET_ATON('$RAD->{FRAMED_IP_ADDRESS}'),
       lupdated=UNIX_TIMESTAMP(),
-      sum=sum+$self->{SUM},
-      acct_input_gigawords='$RAD->{ACCT_INPUT_GIGAWORDS}',
-      acct_output_gigawords='$RAD->{ACCT_OUTPUT_GIGAWORDS}'
+      sum=sum+$self->{SUM}
     WHERE
       acct_session_id=\"$RAD->{ACCT_SESSION_ID}\" and 
       user_name=\"$RAD->{USER_NAME}\" and
       nas_id='$NAS->{NAS_ID}';", 'do');
+
+#      acct_input_octets='$RAD->{INBYTE}',
+#      acct_output_octets='$RAD->{OUTBYTE}',
+#      ex_input_octets='$RAD->{INBYTE2}',
+#      ex_output_octets='$RAD->{OUTBYTE2}',
+#      acct_input_gigawords='$RAD->{ACCT_INPUT_GIGAWORDS}',
+#      acct_output_gigawords='$RAD->{ACCT_OUTPUT_GIGAWORDS}'
+
 
   	return $self;
    }
