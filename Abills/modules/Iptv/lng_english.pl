353f43d401f74825815eba28ec9fe90ae6eb7afb
abills <abills>
2009-04-18 14:03:11 +0000

    *** empty log message ***

--- Abills/modules/Iptv/lng_english.pl
+++ Abills/modules/Iptv/lng_english.pl
@@ -1,18 +1,1 @@
-
-$_FIRST_PERIOD='First period';
-$_FIRST_PERIOD_STEP='Step Of first period';
-$_NEXT_PERIOD='Next Period';
-$_NEXT_PERIOD_STEP='Step of next period';
-$_FREE_TIME='Free Time';
-$_ROUTES='Routes';
-$_ROUTE='Route';
-$_GATEWAY='Gateway';
-$_NUMBER='Number';
-
-$_FAILOVER_TRUNK='Failover Trunk';
-$_PROTOCOL='Protocol';
-$_PROVIDER = 'Provider';
-
-$_ALLOW_ANSWER='Allow answer';
-$_ALLOW_CALLS='Allow Calls';
 $_CLOSE_PERIOD='Close period';
