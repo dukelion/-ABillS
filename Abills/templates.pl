ce590d7c42e137a52ee3f31f800fa8abbe2d5d86
abills <abills>
2009-07-16 15:10:52 +0000

    *** empty log message ***

--- Abills/templates.pl
+++ Abills/templates.pl
@@ -19,11 +19,12 @@ sub _include {
   
   my $sufix = ($attr->{pdf}) ? '.pdf' : '.tpl';
 
+
   if (-f '../../Abills/templates/'.$domain_path. $module . '_' . $tpl . $sufix) {
     return ($FORM{pdf}) ? '../../Abills/templates/'. $domain_path. $module . '_' . $tpl . $sufix : tpl_content('../../Abills/templates/'. $module . '_' . $tpl . $sufix);
    }
   elsif (-f $Bin .'../Abills/templates/'. $module . '_' . $tpl .$sufix) {
-    return ($FORM{pdf}) ? $Bin .'/../Abills/templates/'.$domain_path. $module . '_' . $tpl .$sufix : tpl_content($Bin .'/../Abills/templates/'. $module . '_' . $tpl .$sufix);
+    return ($FORM{pdf}) ? $Bin .'/../Abills/templates/'. $module . '_' . $tpl .$sufix : tpl_content($Bin .'/../Abills/templates/'. $module . '_' . $tpl .$sufix);
    }
   elsif (-f '../Abills/templates/'.$domain_path. $module . '_' . $tpl .$sufix) {
     return ($FORM{pdf}) ? '../Abills/templates/'.$domain_path. $module . '_' . $tpl. '.tpl' : tpl_content('../Abills/templates/'. $module . '_' . $tpl. $sufix);
