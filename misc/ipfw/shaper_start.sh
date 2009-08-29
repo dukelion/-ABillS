90290659ba2f237009797744f09073f2741178f0
abills <abills>
2009-08-29 07:57:46 +0000

    *** empty log message ***

--- misc/ipfw/shaper_start.sh
+++ misc/ipfw/shaper_start.sh
@@ -5,7 +5,7 @@
 #traffic Class numbers
 
 CLASSES_NUMS='2 3'
-VERSION=0.5
+VERSION=0.7
 
 
 
@@ -37,8 +37,14 @@ for num in ${CLASSES_NUMS}; do
 #  FW_NUM=`expr  `;
   echo "Traffic: ${num} "
 
-  ${IPFW} add ` expr 9000 + ${num} \* 10 ` skipto ` expr 10100 + ${num} \* 10 ` ip from table\(` expr ${USER_CLASS_TRAFFIC_NUM} + ${num} \* 2 - 2  `\) to table\(${num}\) in recv ${INTERNAL_INTERFACE}
-  ${IPFW} add ` expr 9000 + ${num} \* 10 + 5` skipto ` expr 10100 + ${num} \* 10 + 5 ` ip from table\(${num}\) to table\(` expr ${USER_CLASS_TRAFFIC_NUM} + ${num} \* 2 - 2 + 1 `\) out xmit ${INTERNAL_INTERFACE}
+  #Unlim traffic
+  ${IPFW} add ` expr 9000 + ${num} \* 10 ` allow ip from table\(9\) to table\(${num}\) in recv ${INTERNAL_INTERFACE}
+  ${IPFW} add ` expr 9000 + ${num} \* 10 + 5 ` allow ip from table\(${num}\) to table\(9\) out xmit ${INTERNAL_INTERFACE}
+
+
+  #Shaped traffic
+  ${IPFW} add ` expr 9100 + ${num} \* 10 ` skipto ` expr 10100 + ${num} \* 10 ` ip from table\(` expr ${USER_CLASS_TRAFFIC_NUM} + ${num} \* 2 - 2  `\) to table\(${num}\) in recv ${INTERNAL_INTERFACE}
+  ${IPFW} add ` expr 9100 + ${num} \* 10 + 5 ` skipto ` expr 10100 + ${num} \* 10 + 5 ` ip from table\(${num}\) to table\(` expr ${USER_CLASS_TRAFFIC_NUM} + ${num} \* 2 - 2 + 1 `\) out xmit ${INTERNAL_INTERFACE}
 
 
   ${IPFW} add ` expr 10100 + ${num} \* 10 ` netgraph tablearg ip from table\(` expr ${USER_CLASS_TRAFFIC_NUM} + ${num} \* 2 - 2  `\) to any in recv ${INTERNAL_INTERFACE}
@@ -49,16 +55,17 @@ for num in ${CLASSES_NUMS}; do
 #  ${IPFW}  add ` expr 9000 + ${num} \* 10 + 5 ` netgraph tablearg ip from table\(${num}\) to table\(` expr ${USER_CLASS_TRAFFIC_NUM} + ${num} \* 2 - 2 + 1 `\) out via ${INTERNAL_INTERFACE}
 done;
 
+  echo "Global shaper"
   ${IPFW}  add 10000 netgraph tablearg ip from table\(10\) to any in recv ${INTERNAL_INTERFACE}
   ${IPFW}  add 10010 netgraph tablearg ip from any to table\(11\) out xmit ${INTERNAL_INTERFACE}
   ${IPFW}  add 10015 allow ip from any to any via ng*
 #done
 else if [ w$1 = wstop ]; then
   for num in ${CLASSES_NUMS}; do
-    ${IPFW} delete ` expr 9000 + ${num} \* 10 ` ` expr 10100 + ${num} \* 10 ` ` expr 9000 + ${num} \* 10 + 5` ` expr 10100 + ${num} \* 10 + 5 `
+    ${IPFW} delete ` expr 9100 + ${num} \* 10 + 5 ` ` expr 9100 + ${num} \* 10 `  ` expr 9000 + ${num} \* 10 ` ` expr 10100 + ${num} \* 10 ` ` expr 9000 + ${num} \* 10 + 5 ` ` expr 10100 + ${num} \* 10 + 5 ` 
   done;
 
-  ${IPFW} delete 10000 10010
+  ${IPFW} delete 10000 10010 10015
 else
   echo "(start|stop)"
 fi;
