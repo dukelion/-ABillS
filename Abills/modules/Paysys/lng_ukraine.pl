4de94b8c5f97d3c9e6acb76e837493e559b93bc3
abills <abills>
2009-10-09 08:17:31 +0000

    *** empty log message ***

---
+++ Abills/modules/Paysys/lng_ukraine.pl
@@ -0,0 +1,9 @@
+$_PAY_SYSTEM='Плат. система';
+$_TRANSACTION='Транзакція';
+$_TEST_MODE='Тестовый режим';
+$_ERR_NO_TRANSACTION='Переказ не зареэстрований';
+$_TRANSACTION_PROCESSING='Платіж обробляєтсья';
+$_FAILED='Транзакація не виконана';
+$ERR_INVALID_SIGNATURE='Неправильний електронний підпис';
+$ERR_TRANSACTION_DECLINED='Транзакція відхилена';
+
