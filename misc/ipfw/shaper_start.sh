57d098769463fb659656a6befff56c0c5425c6d2
abills <abills>
2010-03-17 08:52:17 +0000

    *** empty log message ***

--- misc/ipfw/shaper_start.sh
+++ misc/ipfw/shaper_start.sh
@@ -78,7 +78,7 @@ done;
   ${IPFW} add 10010 netgraph tablearg ip from any to table\(11\) ${OUT_DIRECTION}
   ${IPFW} add 10020 allow ip from table\(9\) to any ${IN_DIRECTION}
   ${IPFW} add 10025 allow ip from any to table\(9\) ${OUT_DIRECTION}
-  ${IPFW} add 10030 allow ip from any to any via ng*
+  ${IPFW} add 10030 allow ip from any to any via ${INTERNAL_INTERFACE} 
 #done
 else if [ w$1 = wstop -a w$2 = w ]; then
 
