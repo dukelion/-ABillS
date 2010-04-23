ddc0d3dc2cf1c584b77f99a72ddb9d84766a9b32
abills <abills>
2010-04-23 07:26:01 +0000

    *** empty log message ***

--- misc/msgs_filter.sh
+++ misc/msgs_filter.sh
@@ -21,15 +21,16 @@ if [ w$1 = wadd ]; then
   SQL="select INET_NTOA(framed_ip_address) from dv_calls WHERE uid IN ($3);";
   OUTPUT=`mysql -D ${MYSQL_DB} -u ${MYSQL_USER} -e "$SQL"`;
 
-  for IP in ${OUTPUT}; do
-    
-    IP=`echo ${IP} | awk '{ print $1 }'`;
-    
+  for LINE in ${OUTPUT}; do
+  
+    IP=`echo ${LINE} | awk '{ print $1 }'`;
+ #   UID=`echo ${LINE} | awk '{ print $2 }'`;
+   
     if [ ${IP} != 'INET_NTOA(framed_ip_address)' ]; then
       if [ w${DEBUG} != w ]; then
-        echo "/sbin/ipfw table ${MSGS_TABLE_NUM} add ${IP}";
+        echo "/sbin/ipfw table ${MSGS_TABLE_NUM} add ${IP} ${UID}";
       fi;
-      /sbin/ipfw table ${MSGS_TABLE_NUM} add ${IP}
+      /sbin/ipfw table ${MSGS_TABLE_NUM} add ${IP} ${UID}
     fi;
     
   done;
