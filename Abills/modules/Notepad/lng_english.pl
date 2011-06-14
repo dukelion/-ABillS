b24934d28366a6dc8cb3b0b48914f91f57f47c3d
abills <abills>
2011-06-14 12:47:22 +0000

    *** empty log message ***

---
+++ Abills/modules/Notepad/lng_english.pl
@@ -0,0 +1,3 @@
+$ERR_FIELDS_FOR_NOTIFIED_AND_SUBJECT_ARE_REQUIRED = 'Date and subject not defined';
+$_ACTIVE = 'Active';
+$_INWORK = 'In work';
---
+++ Abills/modules/Notepad/lng_russian.pl
@@ -0,0 +1,3 @@
+$ERR_FIELDS_FOR_NOTIFIED_AND_SUBJECT_ARE_REQUIRED = 'Поля \'Дата/Время\' \'Тема\' не заполнены';
+$_ACTIVE = 'Открыта';
+$_INWORK = 'В обработке';
