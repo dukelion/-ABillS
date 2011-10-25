3b8c82009a66c52fc96be91773779109ad9a2803
abills <abills>
2011-10-25 14:58:57 +0000

    *** empty log message ***

--- libexec/racct.pl
+++ libexec/racct.pl
@@ -307,7 +307,7 @@ if (-d $conf{extern_acct_dir}) {
     foreach my $file (@contents) {
       if (-x "$conf{extern_acct_dir}/$file" && -f "$conf{extern_acct_dir}/$file") {
         # ACCT_STATUS IP_ADDRESS NAS_PORT
-#        $res = `$conf{extern_acct_dir}/$file $acct_status_type $RAD->{NAS_IP_ADDRESS} $RAD->{NAS_PORT} $nas->{NAS_TYPE} $RAD->{USER_NAME} $RAD->{FRAMED_IP_ADDRESS}`;
+        $res = `$conf{extern_acct_dir}/$file $acct_status_type $RAD->{NAS_IP_ADDRESS} $RAD->{NAS_PORT} $nas->{NAS_TYPE} $RAD->{USER_NAME} $RAD->{FRAMED_IP_ADDRESS}`;
         $Log->log_print('LOG_DEBUG', $RAD->{USER_NAME}, "External accounting program '$conf{extern_acct_dir}' / '$file' pairs '$res'");
        }
      }
@@ -316,7 +316,7 @@ if (-d $conf{extern_acct_dir}) {
       my @pairs = split(/ /, $res);
       foreach my $pair (@pairs) {
         my ($side, $value) = split(/=/, $pair);
-        $RAD->{$side} = "$value";
+        $RAD->{$side} = $value || '';
        }
      }
    }
