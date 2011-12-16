32598633710c2afc7f6ea3ad80a61d50a7ec0cda
abills <abills>
2011-12-16 15:18:04 +0000

    *** empty log message ***

--- Abills/mysql/Voip_Sessions.pm
+++ Abills/mysql/Voip_Sessions.pm
@@ -115,7 +115,7 @@ sub online {
  
  foreach my $line (@$list) {
  	  $dub_logins{$line->[0]}++;
-    push( @{ $nas_sorted{"$line->[7]"} }, [ $line->[0], $line->[1], $line->[2], $line->[3], $line->[4], $line->[5], $line->[6], $line->[7], $line->[8], 
+    push( @{ $nas_sorted{"$line->[8]"} }, [ $line->[0], $line->[1], $line->[2], $line->[3], $line->[4], $line->[5], $line->[6], $line->[7], $line->[8], 
       
       $line->[9], $line->[10], $line->[11], 
       $line->[13], $line->[14], $line->[15], $line->[16], $line->[17], $line->[18], $line->[19], $line->[20], $line->[21], $line->[22]]);
